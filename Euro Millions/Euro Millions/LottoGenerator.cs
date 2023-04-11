caa983fdfc4798467ce26713e1d8e993d8df5f35
Alexander Lairich <alexanderlairich@web.de>
2023-04-11 19:56:58 +0200

    C# terminal project that generates the numbers for Euro Millions
    - Normal section of 5 numbers from 1 to 50
    - Special section of 2 numbers from 1- 12
    - Numbers are unique

---
+++ Euro Millions/Euro Millions/LottoGenerator.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Can you create a small C# terminal project that generates the number for Euro Million
+ * Its 5 numbers from 1 to 50 and 2 stars from 1 to 12
+ * The idea would be to create a program that randomly generates a key for this.
+ */
+
+namespace Euro_Millions
+{
+    public class LottoGenerator
+    {
+
+        /// <summary>
+        /// Generate a hashset of normal and special values
+        /// </summary>
+        /// <returns></returns>
+        public Tuple<HashSet<int>, HashSet<int>> GenerateRandomValues()
+        {
+            Random random = new();
+            HashSet<int> normalNumbers = new();
+            HashSet<int> specialNumbers = new();
+            normalNumbers.Clear();
+            specialNumbers.Clear();
+
+            while (normalNumbers.Count < 5) // generate 5 unique random numbers
+            {
+                int randomNumber = random.Next(1, 50); // generate a random number between 1 and 100
+                if (!normalNumbers.Contains(randomNumber)) // check if the number is already generated
+                {
+                    normalNumbers.Add(randomNumber); // add the number to the HashSet
+                }
+            }
+
+            while (specialNumbers.Count < 2) // generate 2 unique random numbers
+            {
+                int randomNumber = random.Next(1, 12); // generate a random number between 1 and 100
+                if (!specialNumbers.Contains(randomNumber)) // check if the number is already generated
+                {
+                    specialNumbers.Add(randomNumber); // add the number to the HashSet
+                }
+            }
+
+            return new Tuple<HashSet<int>, HashSet<int>>(normalNumbers, specialNumbers);
+        }
+
+
+        /// <summary>
+        /// Return a string of normal and special values
+        /// </summary>
+        /// <param name="hashNormals"></param>
+        /// <param name="hashSpecial"></param>
+        /// <returns></returns>
+        public Tuple<string, string> ShowSelection(HashSet<int> hashNormals, HashSet<int> hashSpecial)
+        {
+            string normals = string.Empty;
+            string specials = string.Empty;
+
+            // Build the normal numbers string
+            for (int i = 0; i < hashNormals.Count; i++)
+            {
+                if (i == hashNormals.Count - 1) normals += hashNormals.ToArray()[i].ToString();
+                else normals += hashNormals.ToArray()[i].ToString() + ",";
+            }
+
+
+            // Build the special numbers string
+            for (int i = 0; i < hashSpecial.Count; i++)
+            {
+                if (i == hashSpecial.Count() - 1) specials += hashSpecial.ToArray()[i].ToString();
+                else specials += hashSpecial.ToArray()[i].ToString() + ",";
+            }
+
+            return new Tuple<string, string>(normals, specials);
+        }
+    }
+}
---
+++ Euro Millions/Euro Millions/Program.cs
@@ -0,0 +1,50 @@
+﻿using Euro_Millions;
+
+
+// Set a title
+Console.Title = "Lotto Generator";
+
+
+// Instantiate the lottogenerator
+LottoGenerator lottoGenerator = new();
+
+
+// Wait for user commanding the start
+Console.WriteLine("PRESS ENTER TO START THE LOTTOMACHINE");
+Console.ReadLine();
+Console.WriteLine("YOUR NUMBERS FOR THIS SESSION:");
+
+
+#region Assign random values
+
+// Return hashsets of normal and special values randomly generated
+Tuple<HashSet<int>, HashSet<int>> SelectionOfToday = lottoGenerator.GenerateRandomValues();
+
+// Assign normal values
+HashSet<int> NormalNumbers = SelectionOfToday.Item1;
+
+// Assign special values
+HashSet<int> SpecialNumbers = SelectionOfToday.Item2;
+
+#endregion Assign random values
+
+
+#region Assign conversation string
+
+// Return string of normal and special string for the conversation
+Tuple<string, string> Selections = lottoGenerator.ShowSelection(NormalNumbers, SpecialNumbers);
+
+// Assign normal string
+string SelectionNormal = Selections.Item1;
+
+// Assign special string
+string SelectionSpecial = Selections.Item2;
+
+#endregion Assign conversation string
+
+
+// Print selection to console
+Console.WriteLine($"Normal: {SelectionNormal}\nSpecial: {SelectionSpecial}");
+
+
+while (true) Console.ReadLine();
